e0d1fddd6e753a7c8ee3b21e3151beacb6403db6
Matt Pryor <matt@stackhpc.com>
2023-08-11 17:59:32 +0100

    Initial commit

---
+++ capi_janitor/openstack/openstack.py
@@ -0,0 +1,193 @@
+import asyncio
+import contextlib
+import urllib.parse
+
+import httpx
+
+from easykube import rest
+
+
+class UnsupportedAuthenticationError(Exception):
+    """
+    Raised when an unsupported authentication method is used.
+    """
+    def __init__(self, auth_type):
+        super().__init__(f"unsupported authentication type: {auth_type}")
+
+
+class Auth(httpx.Auth):
+    """
+    Authenticator class for OpenStack connections.
+    """
+    def __init__(self, auth_url, application_credential_id, application_credential_secret):
+        self.url = auth_url
+        self._application_credential_id = application_credential_id
+        self._application_credential_secret = application_credential_secret
+        self._token = None
+        self._lock = asyncio.Lock()
+
+    @contextlib.asynccontextmanager
+    async def _refresh_token(self):
+        """
+        Context manager to ensure only one request at a time triggers a token refresh.
+        """
+        token = self._token
+        async with self._lock:
+            # Only yield to the wrapped block if the token has not changed
+            # in the time it took to acquire the lock
+            if token == self._token:
+                yield
+
+    def _build_token_request(self):
+        return httpx.Request(
+            "POST",
+            f"{self.url}/v3/auth/tokens",
+            json = {
+                "auth": {
+                    "identity": {
+                        "methods": ["application_credential"],
+                        "application_credential": {
+                            "id": self._application_credential_id,
+                            "secret": self._application_credential_secret,
+                        },
+                    },
+                },
+            }
+        )
+    
+    def _handle_token_response(self, response):
+        response.raise_for_status()
+        self._token = response.headers["X-Subject-Token"]
+
+    async def async_auth_flow(self, request):
+        if self._token is None:
+            async with self._refresh_token():
+                response = yield self._build_token_request()
+                self._handle_token_response(response)
+        request.headers['X-Auth-Token'] = self._token
+        response = yield request
+
+
+class Resource(rest.Resource):
+    """
+    Base resource for OpenStack APIs.
+    """
+    def __init__(self, client, name, prefix = None, plural_name = None, singular_name = None):
+        super().__init__(client, name, prefix)
+        # Some resources support a /detail endpoint
+        # In this case, we just want to use the name up to the slash as the plural name
+        self._plural_name = plural_name or self._name.split("/")[0]
+        # If no singular name is given, assume the name ends in 's'
+        self._singular_name = singular_name or self._plural_name[:-1]
+
+    def _extract_list(self, response):
+        # Some resources support a /detail endpoint
+        # In this case, we just want to use the name up to the slash
+        return response.json()[self._plural_name]
+    
+    def _extract_next_page(self, response):
+        next_url = next(
+            (
+                link["href"]
+                for link in response.json().get(f"{self._plural_name}_links", [])
+                if link["rel"] == "next"
+            ),
+            None
+        )
+        # Sometimes, the returned URLs have http where they should have https
+        # To mitigate this, we split the URL and return the path and params separately
+        url = urllib.parse.urlsplit(next_url)
+        params = urllib.parse.parse_qs(url.query)
+        return url.path, params
+
+    def _extract_one(self, response):
+        content_type = response.headers.get("content-type")
+        if content_type == "application/json":
+            return response.json()[self._singular_name]
+        else:
+            return super()._extract_one(response)
+
+
+class Client(rest.AsyncClient):
+    """
+    Client for OpenStack APIs.
+    """
+    def __init__(self, /, base_url, prefix = None, **kwargs):
+        # Extract the path part of the base_url
+        url = urllib.parse.urlsplit(base_url)
+        # Initialise the client with the scheme/host
+        super().__init__(base_url = f"{url.scheme}://{url.netloc}", **kwargs)
+        # If another prefix is not given, use the path from the base URL as the prefix
+        # This ensures things like pagination work nicely without duplicating the prefix
+        self._prefix = prefix or url.path
+
+    def __aenter__(self):
+        # Prevent individual clients from being used in a context manager
+        raise RuntimeError("clients must be used via a cloud object")
+    
+    def resource(self, name, plural_name = None, singular_name = None):
+        return Resource(self, name, self._prefix, plural_name, singular_name)
+
+
+class Cloud:
+    """
+    Object for interacting with OpenStack clouds.
+    """
+    def __init__(self, auth, transport, interface):
+        self._auth = auth
+        self._transport = transport
+        self._interface = interface
+        self._endpoints = {}
+        # A map of api name to client
+        self._clients = {}
+
+    async def __aenter__(self):
+        await self._transport.__aenter__()
+        # Once the transport has been initialised, we can initialise the endpoints
+        client = Client(base_url = self._auth.url, auth = self._auth, transport = self._transport)
+        response = await client.get("/v3/auth/catalog")
+        self._endpoints = {
+            entry["type"]: next(
+                ep["url"]
+                for ep in entry["endpoints"]
+                if ep["interface"] == self._interface
+            )
+            for entry in response.json()["catalog"]
+        }
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self._transport.__aexit__(exc_type, exc_value, traceback)
+
+    @property
+    def apis(self):
+        """
+        The APIs supported by the cloud.
+        """
+        return list(self._endpoints.keys())
+
+    def api_client(self, name, prefix = None):
+        """
+        Returns a client for the named API.
+        """
+        if name not in self._clients:
+            self._clients[name] = Client(
+                base_url = self._endpoints[name],
+                prefix = prefix,
+                auth = self._auth,
+                transport = self._transport
+            )
+        return self._clients[name]
+
+    @classmethod
+    def from_clouds(cls, clouds, cloud = "openstack"):
+        config = clouds["clouds"][cloud]
+        if config["auth_type"] != "v3applicationcredential":
+            raise UnsupportedAuthenticationError(config["auth_type"])
+        auth = Auth(
+            config["auth"]["auth_url"],
+            config["auth"]["application_credential_id"],
+            config["auth"]["application_credential_secret"]
+        )
+        transport = httpx.AsyncHTTPTransport(verify = config.get("verify", True))
+        return cls(auth, transport, config.get("interface", "public"))
---
+++ capi_janitor/openstack/operator.py
@@ -0,0 +1,256 @@
+import asyncio
+import base64
+import functools
+import os
+import random
+
+import kopf
+import yaml
+
+import easykube
+
+from . import openstack
+
+
+ekconfig = easykube.Configuration.from_environment()
+ekclient = ekconfig.async_client()
+
+
+CAPO_VERSION = "infrastructure.cluster.x-k8s.io/v1alpha6"
+FINALIZER = "janitor.capi.stackhpc.com"
+
+VOLUMES_ANNOTATION = "janitor.capi.stackhpc.com/volumes"
+VOLUMES_ANNOTATION_KEEP = "keep"
+VOLUMES_ANNOTATION_DEFAULT = os.environ.get("CAPI_JANITOR_VOLUMES_ANNOTATION_DEFAULT", "remove")
+
+RETRY_ANNOTATION = "janitor.capi.stackhpc.com/retry"
+RETRY_MAX_BACKOFF = int(os.environ.get("CAPI_JANITOR_RETRY_MAX_BACKOFF", "60"))
+
+
+@kopf.on.cleanup()
+async def on_cleanup(**kwargs):
+    """
+    Runs on operator shutdown.
+    """
+    # Make sure that the easykube client is shut down properly
+    await ekclient.aclose()
+
+
+class ResourcesStillPresentError(Exception):
+    """
+    Raised when cluster resources are still present even after being deleted,
+    e.g. while waiting for deletion.
+    """
+    def __init__(self, resource, cluster):
+        super().__init__(f"{resource} still present for cluster {cluster}")
+
+
+async def fips_for_cluster(resource, cluster):
+    """
+    Async iterator for FIPs belonging to the specified cluster.
+    """
+    async for fip in resource.list():
+        if not fip.description.startswith("Floating IP for Kubernetes external service"):
+            continue
+        if not fip.description.endswith(f"from cluster {cluster}"):
+            continue
+        yield fip
+
+
+async def lbs_for_cluster(resource, cluster):
+    """
+    Async iterator for loadbalancers belonging to the specified cluster.
+    """
+    async for lb in resource.list():
+        if lb.name.startswith(f"kube_service_{cluster}_"):
+            yield lb
+
+
+async def volumes_for_cluster(resource, cluster):
+    """
+    Async iterator for volumes belonging to the specified cluster.
+    """
+    async for vol in resource.list():
+        # CSI Cinder sets metadata on the volumes that we can look for
+        owner = vol.metadata.get("cinder.csi.openstack.org/cluster")
+        if owner and owner == cluster:
+            yield vol
+
+
+async def empty(async_iterator):
+    """
+    Returns True if the given async iterator is empty, False otherwise.
+    """
+    try:
+        _ = await async_iterator.__anext__()
+    except StopAsyncIteration:
+        return True
+    else:
+        return False
+    
+
+async def patch_finalizers(resource, name, namespace, finalizers):
+    """
+    Patches the finalizers of a resource. If the resource does not exist any
+    more, that is classed as a success.
+    """
+    try:
+        await resource.patch(
+            name,
+            { "metadata": { "finalizers": finalizers } },
+            namespace = namespace
+        )
+    except easykube.ApiError as exc:
+        if exc.status_code != 404:
+            raise
+    
+
+def retry_event(handler):
+    """
+    Decorator for retrying events on Kubernetes objects.
+
+    Objects are annotated with the number of times the handler has been retried
+    since it was last successful, which is used to calculate an exponential backoff.
+
+    Doing it this way ensures that the handler sees the most recent data each time.
+
+    As recommended in the kopf docs, handlers should be idempotent as this mechanism
+    may result in the handler being called twice if an update happens between a failure
+    and the associated retry.
+    """
+    @functools.wraps(handler)
+    async def wrapper(**kwargs):
+        body = kwargs["body"]
+        resource = await ekclient.api(body["apiVersion"]).resource(body["kind"])
+        try:
+            result = await handler(**kwargs)
+        except Exception as exc:
+            kwargs["logger"].exception(str(exc))
+            # Check to see how many times a handler has been retried
+            retries = int(kwargs["annotations"].get(RETRY_ANNOTATION, "0"))
+            # Calculate the backoff
+            backoff = 2**retries + random.uniform(0, 1)
+            clamped_backoff = min(backoff, RETRY_MAX_BACKOFF)
+            # Wait for the backoff before annotating the resource
+            await asyncio.sleep(clamped_backoff)
+            await resource.patch(
+                kwargs["name"],
+                {
+                    "metadata": {
+                        "annotations": {
+                            RETRY_ANNOTATION: str(retries + 1),
+                        }
+                    }
+                },
+                namespace = kwargs["namespace"]
+            )
+        else:
+            if RETRY_ANNOTATION in kwargs["annotations"]:
+                # If the handler completes successfully, ensure the annotation is removed
+                # The forward slash in the annotation is designated by '~1' in JSON patch
+                annotation = RETRY_ANNOTATION.replace('/', '~1')
+                await resource.json_patch(
+                    kwargs["name"],
+                    [
+                        {
+                            "op": "remove",
+                            "path": f"/metadata/annotations/{annotation}",
+                        },
+                    ],
+                    namespace = kwargs["namespace"]
+                )
+        return result
+    return wrapper
+
+
+@kopf.on.event(CAPO_VERSION, "openstackclusters")
+@retry_event
+async def on_openstackcluster_event(type, name, namespace, meta, spec, **kwargs):
+    """
+    Executes whenever an event occurs for an OpenStack cluster.
+    """
+    print(type)
+    print(meta.get("finalizers"))
+    print(meta.get("annotations"))
+    finalizers = meta.get("finalizers", [])
+    # We add a custom finalizer to OpenStack cluster objects to
+    # prevent them from being deleted until we have acted
+    if not meta.get("deletionTimestamp"):
+        if FINALIZER not in finalizers:
+            openstackclusters = await ekclient.api(CAPO_VERSION).resource("openstackclusters")
+            await patch_finalizers(
+                openstackclusters,
+                name,
+                namespace,
+                finalizers + [FINALIZER]
+            )
+        return
+    
+    # If we are being deleted but the finalizer has already been removed,
+    # then there is nothing to do
+    if FINALIZER not in finalizers:
+        return
+    
+    # Get the cloud credential from the cluster
+    secrets = await ekclient.api("v1").resource("secrets")
+    clouds_secret = await secrets.fetch(spec["identityRef"]["name"], namespace = namespace)
+    clouds = yaml.safe_load(base64.b64decode(clouds_secret.data["clouds.yaml"]))
+    if "cacert" in clouds_secret.data:
+        cacert = base64.b64decode(clouds_secret.data["cacert"])
+    else:
+        cacert = None
+
+    # Use the credential to delete external resources as required
+    async with openstack.Cloud.from_clouds(clouds) as cloud:
+        # Release any floating IPs associated with loadbalancer services for the cluster
+        networkapi = cloud.api_client("network", "/v2.0/")
+        fips = networkapi.resource("floatingips")
+        check_fips = True
+        async for fip in fips_for_cluster(fips, name):
+            await fips.delete(fip.id)
+        else:
+            check_fips = False
+
+        # Delete any loadbalancers associated with loadbalancer services for the cluster
+        lbapi = cloud.api_client("load-balancer", "/v2/lbaas/")
+        loadbalancers = lbapi.resource("loadbalancers")
+        check_lbs = True
+        async for lb in lbs_for_cluster(loadbalancers, name):
+            if lb.provisioning_status not in {"PENDING_DELETE", "DELETED"}:
+                await loadbalancers.delete(lb.id, cascade = "true")
+        else:
+            check_lbs = False
+
+        # Delete volumes associated with PVCs, unless requested otherwise via the annotation
+        volumeapi = cloud.api_client("volumev3")
+        volumes_detail = volumeapi.resource("volumes/detail")
+        volumes = volumeapi.resource("volumes")
+        check_volumes = False
+        volumes_annotation_value = meta.get("annotations", {}).get(
+            VOLUMES_ANNOTATION,
+            VOLUMES_ANNOTATION_DEFAULT
+        )
+        if volumes_annotation_value != VOLUMES_ANNOTATION_KEEP:
+            check_volumes = True
+            async for vol in volumes_for_cluster(volumes_detail, name):
+                if vol.status != "deleting":
+                    await volumes.delete(vol.id)
+            else:
+                check_volumes = False
+
+        # Check that the resources have actually been deleted
+        if check_fips and not await empty(fips_for_cluster(fips, name)):
+            raise ResourcesStillPresentError("floatingips", name)
+        if check_lbs and not await empty(lbs_for_cluster(loadbalancers, name)):
+            raise ResourcesStillPresentError("loadbalancers", name)
+        if check_volumes and not await empty(volumes_for_cluster(volumes_detail, name)):
+            raise ResourcesStillPresentError("volumes", name)
+        
+    # If we get to here, we can remove the finalizer
+    openstackclusters = await ekclient.api(CAPO_VERSION).resource("openstackclusters")
+    await patch_finalizers(
+        openstackclusters,
+        name,
+        namespace,
+        [f for f in finalizers if f != FINALIZER]
+    )
