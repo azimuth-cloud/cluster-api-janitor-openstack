f13e361c495d063c1f3c36d5b4a17b84016386d0
Matt Anson <matta@stackhpc.com>
2026-07-28 18:58:26 +0100

    Revert "feat: rewrite operator in Go (Kubebuilder v4) (#298)"
    This reverts commit 764a1b0e349881982a1b3496ab7874564861659d.

--- nix/nixpkgs.nix
+++
@@ -1,5 +0,0 @@
-import (builtins.fetchTarball {
-  # nixos-26.05 — require Go 1.26+
-  # To update: nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/<rev>.tar.gz
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-26.05.tar.gz";
-}) {}
